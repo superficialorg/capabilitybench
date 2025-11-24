fabc5bd718f68ddef04f8473ca02d32358b6f95a
superficialorg <david@general.ventures>
2025-11-25 08:19:56 +1000

    Right-align navigation links while keeping —Superficial on the left

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -21,13 +21,13 @@ export function Navbar() {
     <aside className="mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start justify-end relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row items-start justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row items-center space-x-0 pr-10 flex-nowrap overflow-x-auto">
-            <Link href="/" className="flex items-center align-middle relative py-1 mr-6 pl-0">
-              <span className={`${inter.className} font-bold text-xl`}>—Superficial</span>
-            </Link>
+          <Link href="/" className="flex items-center align-middle relative py-1 pl-0">
+            <span className={`${inter.className} font-bold text-xl`}>—Superficial</span>
+          </Link>
+          <div className="flex flex-row items-center space-x-0 flex-nowrap overflow-x-auto">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
