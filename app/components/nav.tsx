2804d4b367b815ef09188c2d3d47616512c02d70
superficialorg <david@general.ventures>
2025-12-07 15:35:40 +1000

    Rewrite CAPE blog post with new content, improved structure, and nicely formatted tables

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,40 +1,32 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 const navItems = {
-  '/research': {
-    name: 'research',
+  '/': {
+    name: 'home',
   },
-  '/joinus': {
-    name: 'join us',
+  '/blog': {
+    name: 'blog',
   },
-  '/cape': {
-    name: 'cape',
+  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
+    name: 'deploy',
   },
 }
 
 export function Navbar() {
   return (
-    <aside className="mb-16 tracking-tight">
+    <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-center justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <Link href="/" className="flex items-center align-middle relative py-1 pl-0 gap-2">
-            <Image src="/Logo1.png" alt="Superficial Logo" width={23} height={23} />
-            <span className={`${inter.className} font-semibold text-lg`}>Superficial</span>
-          </Link>
-          <div className="flex flex-row items-center space-x-0 flex-nowrap overflow-x-auto">
+          <div className="flex flex-row space-x-0 pr-10">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 flex align-middle relative py-1 px-2 m-1 text-black whitespace-nowrap"
+                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
                 >
                   {name}
                 </Link>
