157529a15f06a68f9dfc4ae7593d33fb7a3c14d1
superficialorg <david@general.ventures>
2025-12-08 12:20:59 +1000

    Change nav links to lowercase

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -6,13 +6,13 @@ const inter = Inter({ subsets: ['latin'] })
 
 const navItems = {
   '/research': {
-    name: 'Research',
+    name: 'research',
   },
   '/cape': {
-    name: 'CAPE',
+    name: 'cape',
   },
   '/capabilitybench': {
-    name: 'CapabilityBench',
+    name: 'capabilitybench',
   },
 }
 
